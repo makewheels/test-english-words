4e3a383841bd31a9728599d7d9b47af1bfaca899
makewheels <makewheels>
2021-04-24 19:25:53 +0800

    可以了

--- src/main/java/com/eg/testenglishwords/Test.java
+++ src/main/java/com/eg/testenglishwords/Test.java
@@ -34,39 +34,57 @@ public class Test {
     }
 
     public static File getAudioFolder() {
-        return new File("C:/Users/thedoflin/Downloads/audio");
+        return new File("C:/Users/thedoflin/Downloads/youdao");
     }
 
-    public static boolean downloadAndCompareMd5(Set<String> md5List, String url, File file) throws IOException {
-        FileUtils.copyURLToFile(new URL(url), file);
-        System.out.println(Thread.currentThread().getName() + " download " + url);
+    public static String downloadAndCompareMd5(Set<String> md5List, String url, File file)
+            throws IOException {
+        try {
+            FileUtils.copyURLToFile(new URL(url), file);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            try {
+                FileUtils.copyURLToFile(new URL(url), file);
+            } catch (IOException e2) {
+                e2.printStackTrace();
+                FileUtils.copyURLToFile(new URL(url), file);
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " " + url);
         FileInputStream fileInputStream = new FileInputStream(file);
         String md5 = DigestUtils.md5Hex(fileInputStream);
-        System.out.println(md5);
         fileInputStream.close();
         if (md5List.contains(md5)) {
             boolean deleteResult = file.delete();
             System.out.println("file.delete() " + file.getName() + " " + deleteResult);
-            return false;
         }
         md5List.add(md5);
-        return true;
+        return md5;
     }
 
     public static void downloadAudio(String word) throws IOException {
         Set<String> md5List = new HashSet<>();
         for (int type = 1; type <= 2; type++) {
             String url = getAudioDownloadUrl(word, type + "");
+
+            String which;
+            if (type == 1)
+                which = "uk";
+            else which = "us";
+
             File audioFile = new File(getAudioFolder() + "/" + word.charAt(0) + "/"
-                    + word + "/" + word + "-" + type + ".mp3");
-            downloadAndCompareMd5(md5List, url, audioFile);
+                    + word + "/" + which + "/" + word + ".mp3");
+            String md5 = downloadAndCompareMd5(md5List, url, audioFile);
+            File renameFile = new File(audioFile.getParent(), md5 + ".mp3");
+            audioFile.renameTo(renameFile);
+            System.out.println(renameFile);
         }
     }
 
     //    https://fanyi.baidu.com/gettts?lan=en&text=abstract&spd=3&source=web
     public static void main(String[] args) {
         List<String> words = readWords();
-        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
         words.forEach(word -> executorService.submit(() -> {
             try {
                 downloadAudio(word);
